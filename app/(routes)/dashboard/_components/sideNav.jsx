55ab73e0149a61cd325b6a8080ed9b95c665bdb7
Noman Patel <golutsuto@Nomans-Mac-mini.local>
2024-07-22 09:44:38 -0400

    Removed SideNav from mobile screens

--- app/(routes)/dashboard/_components/sideNav.jsx
+++ app/(routes)/dashboard/_components/sideNav.jsx
@@ -1,67 +1,65 @@
-"use client"
-import React, { useEffect } from 'react'
-import Image from 'next/image'
-import { LayoutDashboardIcon, PiggyBank, ReceiptIcon, ShieldCheckIcon } from 'lucide-react'
-import { UserButton } from '@clerk/nextjs'
-import { usePathname } from 'next/navigation'
-import Link from 'next/link'
+"use client";
+import React, { useEffect } from 'react';
+import Image from 'next/image';
+import { LayoutDashboardIcon, PiggyBank, ReceiptIcon } from 'lucide-react';
+import { UserButton } from '@clerk/nextjs';
+import { usePathname } from 'next/navigation';
+import Link from 'next/link';
 
-
-function sideNav(){
-  const  menuList=[
+function sideNav() {
+  const menuList = [
     {
-    id:1,
-    name:"Dashboard",
-    icon:LayoutDashboardIcon,
-    path:'/dashboard'
+      id: 1,
+      name: "Dashboard",
+      icon: LayoutDashboardIcon,
+      path: '/dashboard'
     },
     {
-      id:2,
-      name:"Budgets",
-      icon:PiggyBank,
-      path:'/dashboard/budgets'
+      id: 2,
+      name: "Budgets",
+      icon: PiggyBank,
+      path: '/dashboard/budgets'
     },
     {
-      id:3,
-      name:"Expenses",
-      icon:ReceiptIcon,
-      path:'/dashboard/expenses'
+      id: 3,
+      name: "Expenses",
+      icon: ReceiptIcon,
+      path: '/dashboard/expenses'
     }
-  ]
+  ];
 
-  const path = usePathname()
+  const path = usePathname();
 
-  useEffect(() =>{
-    console.log(path)
-  },[path])
-  
+  useEffect(() => {
+    console.log(path);
+  }, [path]);
+
   return (
-    <div className='h-screen p-5 border shadow-sm'>
+    <div className='h-screen p-5 border shadow-sm fixed md:block hidden'>
       <Image src='/expense_tracker_logo.svg' 
-      alt='logo'
-      width={100}
-      height={100}
+        alt='logo'
+        width={100}
+        height={100}
       />
 
       <div className='mt-5'>
-        {menuList.map((menu,index) =>(
-          <Link href={menu.path}>
+        {menuList.map((menu, index) => (
+          <Link href={menu.path} key={menu.id}>
             <h2 className={`flex gap-2 items-center text-gray-500 font-medium p-5 mb-1
                             cursor-pointer rounded-md hover:text-customBlue hover:bg-blue-300
-                            ${path==menu.path && 'text-customBlue bg-blue-100'}`}>
+                            ${path == menu.path && 'text-customBlue bg-blue-100'}`}>
               <menu.icon size={20} />
               {menu.name}
             </h2>
           </Link>
-          
         ))}
       </div>
       <div className='fixed bottom-10 p-5 flex gap-2 items-center'>
-        <UserButton/>
+        <UserButton />
         Profile
       </div>
     </div>
-  )
+  );
 }
 
-export default sideNav
+export default sideNav;
